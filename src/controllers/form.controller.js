57f4d1483a220b1f2a43210c349116070d14ea84
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-06-01 21:42:25 +0700

    Add layout template and add to form

--- src/controllers/form.controller.js
+++ src/controllers/form.controller.js
@@ -1,9 +1,9 @@
 function getSiteHandler(req, res) {
-  res.render('form');
+  res.render('form', {title: 'Registration Form'});
 }
 
 function postFormHandler(req, res) {
-  res.render('form');
+  res.render('form', {title: 'Registration Form'});
 }
 
 module.exports = { getSiteHandler, postFormHandler };
