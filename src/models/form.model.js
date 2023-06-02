c326af9e5252d43855997644752abb186f40260f
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-06-02 15:36:07 +0700

    Add some validator to Schema Mongoose

--- src/models/form.model.js
+++ src/models/form.model.js
@@ -4,10 +4,12 @@ const formDataSchema = new mongoose.Schema({
   name: {
     type: String,
     trim: true,
+    required: true,
   },
   email: {
     type: String,
     trim: true,
+    required: true,
   }
 });
 
