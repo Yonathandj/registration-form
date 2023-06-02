ed26f8d00e22618ab44f4a10e7d9494db697d190
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-06-02 15:29:08 +0700

    Add Schema & Model and send data to database

--- src/controllers/form.controller.js
+++ src/controllers/form.controller.js
@@ -1,19 +1,22 @@
-const formModel = require('./../models/form.model');
+const Registration = require('./../models/form.model');
 
 function getSiteHandler(req, res) {
   res.render('form', {title: 'Registration Form'});
 }
-function postFormHandler(req, res) {
-  const { name, email } = req.body;
-  const newData = {
-    name,
-    email,
-  };
-  formModel.push(newData);
-  res.status(201).render('form', {
-    title: 'Registration Form',
-    data: req.body,
-  });
+async function postFormHandler(req, res) {
+  try {
+    const registration = new Registration(req.body);
+    registration.save();
+    res.status(201).render('form', {
+      title: 'Registration Form',
+      data: req.body,
+    });
+  } catch (err) {
+    res.status(500).render('form', {
+      title: 'Registration Form',
+    });
+  }
 }
+  
 
 module.exports = { getSiteHandler, postFormHandler };
--- src/models/form.model.js
+++ src/models/form.model.js
@@ -1,2 +1,15 @@
-const formModel = [];
-module.exports = formModel;
+const mongoose = require('mongoose');
+
+const formDataSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+  }
+});
+
+const Registration = mongoose.model('Registration', formDataSchema);
+module.exports = Registration;
