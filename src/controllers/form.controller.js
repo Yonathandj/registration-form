63ed7543710879b34bcb549bfa53fc29257b5bdb
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-06-02 07:58:13 +0700

    Add temporary model for form Data & add as object

--- src/controllers/form.controller.js
+++ src/controllers/form.controller.js
@@ -1,8 +1,18 @@
+const formModel = require('./../models/form.model');
+
 function getSiteHandler(req, res) {
+  
   res.render('form', {title: 'Registration Form'});
 }
-
+``
 function postFormHandler(req, res) {
+  const { name, email } = req.body;
+  const newData = {
+    name,
+    email,
+  }
+  formModel.push(newData);
+  console.log(formModel);
   res.render('form', {title: 'Registration Form'});
 }
 
---
+++ src/models/form.model.js
@@ -0,0 +1,2 @@
+const formModel = [];
+module.exports = formModel;
