8c56a0131c1d64307d70417ffffb3b9b8b84fe8e
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-06-03 09:44:59 +0700

    Add basic auth path /registrations with http-auth

--- src/routes/form.route.js
+++ src/routes/form.route.js
@@ -1,13 +1,21 @@
+const path = require('path');
+const auth = require('http-auth');
 const express = require('express');
+const authConnect = require('http-auth-connect');
+
 const formRoute = express.Router();
 
-const { getSiteHandler, postFormHandler, getRegistrationsHandler } = require('../controllers/form.controller');
 const validateUser = require('./../validators/form.validator'); 
+const { getSiteHandler, postFormHandler, getRegistrationsHandler } = require('../controllers/form.controller');
+
+const basic = auth.basic({
+  file: path.join(__dirname, './../../users.htpasswd')
+});
 
 formRoute.route('/')
   .get(getSiteHandler)
   .post(validateUser, postFormHandler);
 
-formRoute.get('/registrations', getRegistrationsHandler);
+formRoute.get('/registrations', authConnect(basic), getRegistrationsHandler);
 
 module.exports = formRoute;
